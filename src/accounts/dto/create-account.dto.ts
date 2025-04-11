7296ab1995a6a1ac282d9b96fe3eb7649b5d702d
ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2025-04-11 11:14:13 +0100

    add DTO parameters

--- src/accounts/dto/create-account.dto.ts
+++ src/accounts/dto/create-account.dto.ts
@@ -1,1 +1,11 @@
-export class CreateAccountDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator"
+
+export class CreateAccountDto {
+    
+    @IsNotEmpty()
+    @IsString()
+    owner: string
+
+    @IsNumber()
+    balance: string
+}
--- src/entries/dto/create-entry.dto.ts
+++ src/entries/dto/create-entry.dto.ts
@@ -1,1 +1,11 @@
-export class CreateEntryDto {}
+import { IsNotEmpty, IsNumber } from "class-validator"
+
+export class CreateEntryDto {
+    @IsNumber()
+    @IsNotEmpty()
+    amount: string
+
+    @IsNumber()
+    @IsNotEmpty()
+    account_id: string
+}
