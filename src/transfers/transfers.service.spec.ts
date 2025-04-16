924a951bf9d92747df92dc644b39fcaad489d41d
ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2025-04-16 03:10:13 +0100

    checkpoint: make the transfer service tests default tests pass

--- src/transfers/transfers.service.spec.ts
+++ src/transfers/transfers.service.spec.ts
@@ -1,16 +1,28 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { TestingModule } from '@nestjs/testing';
 import { TransfersService } from './transfers.service';
+import { AccountsModule } from '../accounts/accounts.module';
+import { Transfer } from './entities/transfer.entity';
+import { Account } from '../accounts/entities/account.entity';
+import { Entry } from '../entries/entities/entry.entity';
+import { createTestModule } from '../../test/test_service_util';
 
 describe('TransfersService', () => {
   let service: TransfersService;
+  let module: TestingModule
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    module = await createTestModule({
+      entities: [Transfer, Account, Entry],
       providers: [TransfersService],
-    }).compile();
+      imports: [AccountsModule]
+    })
 
     service = module.get<TransfersService>(TransfersService);
   });
+
+  afterAll(async () => {
+    await module.close()
+  })
 
   it('should be defined', () => {
     expect(service).toBeDefined();
