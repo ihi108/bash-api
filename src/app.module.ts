e777d9bbab87c5684c14cfe41c5e939949764b7d
ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2025-04-16 04:53:50 +0100

    checkpoint: use nest ConfigService to access configurations from .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,24 +7,31 @@ import { EntriesModule } from './entries/entries.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     AccountsModule, 
     TransfersModule, 
     EntriesModule, 
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "root",
-      password: "password",
-      database: "bash",
-      synchronize: true,
-      entities: [],
-      autoLoadEntities: true,
-      logging: true,
-    }), UsersModule
+    UsersModule,
+    ConfigModule.forRoot({isGlobal:true}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get<string>("DB_HOST"),
+        port: configService.get<number>("DB_PORT"),
+        username: configService.get<string>("DB_USER"),
+        password: configService.get<string>("DB_PASSWORD"),
+        database: configService.get<string>("DB_NAME"),
+        synchronize: true,
+        entities: [],
+        autoLoadEntities: true,
+        logging: true,
+      }),
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
