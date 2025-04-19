8cdcf7a6f705b02f41572ca9192d5c88aff21156
ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2025-04-19 03:58:09 +0100

    checkpoint: implement transaction retries due to serializable isolation level

--- src/transfers/transfers.service.ts
+++ src/transfers/transfers.service.ts
@@ -15,63 +15,72 @@ export class TransfersService {
   ) {}
 
   async create(createTransferDto: CreateTransferDto): Promise<responseTransferDto> {
-
+    const retries:number = 5;
     let res: responseTransferDto;
-    const queryRunner = this.dataSource.createQueryRunner()
-    await queryRunner.connect()
-    await queryRunner.startTransaction("SERIALIZABLE")
 
-    try {
-      const fromEntry: Entry = new Entry()
-      fromEntry.amount = - createTransferDto.amount;
-      fromEntry.account_id = String(createTransferDto.from_account_id);
+    for (let attempts = 0; attempts < retries; attempts++) {
+      const queryRunner = this.dataSource.createQueryRunner()
+      await queryRunner.connect()
+      await queryRunner.startTransaction("SERIALIZABLE")
 
-      const toEntry: Entry = new Entry()
-      toEntry.amount = createTransferDto.amount;
-      toEntry.account_id = String(createTransferDto.to_account_id);
+      try {
+        const fromEntry: Entry = new Entry()
+        fromEntry.amount = - createTransferDto.amount;
+        fromEntry.account_id = String(createTransferDto.from_account_id);
+
+        const toEntry: Entry = new Entry()
+        toEntry.amount = createTransferDto.amount;
+        toEntry.account_id = String(createTransferDto.to_account_id);
 
 
-      const fromAccount = await this.accountService.findOne(createTransferDto.from_account_id);
-      if (!fromAccount) {
-        throw new NotFoundException(`account with id: ${createTransferDto.from_account_id} not found`)
+        const fromAccount = await this.accountService.findOne(createTransferDto.from_account_id);
+        if (!fromAccount) {
+          throw new NotFoundException(`account with id: ${createTransferDto.from_account_id} not found`)
+        }
+        if (fromAccount.balance < createTransferDto.amount) {
+          throw new ForbiddenException(`fromAccount has insufficient funds`);
+        }
+        fromAccount.balance = Number(fromAccount.balance) - Number(createTransferDto.amount);
+
+        const toAccount = await this.accountService.findOne(createTransferDto.to_account_id);
+        if (!toAccount) {
+          throw new NotFoundException(`account with id: ${createTransferDto.from_account_id} not found`)
+        }
+        toAccount.balance = Number(toAccount.balance) + Number(createTransferDto.amount);
+
+
+        const transfer = new Transfer()
+        transfer.from_account_id = createTransferDto.from_account_id;
+        transfer.to_account_id = createTransferDto.to_account_id;
+        transfer.amount = createTransferDto.amount;
+
+        await queryRunner.manager.save(fromEntry)
+        await queryRunner.manager.save(toEntry)
+        await queryRunner.manager.save(fromAccount)
+        await queryRunner.manager.save(toAccount)
+        await queryRunner.manager.save(transfer)
+
+        await queryRunner.commitTransaction()
+
+        res = {
+          from_account: fromAccount,
+          to_account: toAccount,
+          from_entry: fromEntry,
+          to_entry: toEntry,
+          transfer: transfer
+        }
+      } catch (error) {
+        await queryRunner.rollbackTransaction()
+        if (error.code == 40001 && attempts < retries - 1) {
+          await new Promise(res => setTimeout(res, 100 * (attempts + 1)));
+          console.log("Retrying transaction");
+        } else {
+          throw error
+        }
+      } finally {
+        await queryRunner.release()
+        break
       }
-      if (fromAccount.balance < createTransferDto.amount) {
-        throw new ForbiddenException(`fromAccount has insufficient funds`);
-      }
-      fromAccount.balance = Number(fromAccount.balance) - Number(createTransferDto.amount);
-
-      const toAccount = await this.accountService.findOne(createTransferDto.to_account_id);
-      if (!toAccount) {
-        throw new NotFoundException(`account with id: ${createTransferDto.from_account_id} not found`)
-      }
-      toAccount.balance = Number(toAccount.balance) + Number(createTransferDto.amount);
-
-
-      const transfer = new Transfer()
-      transfer.from_account_id = createTransferDto.from_account_id;
-      transfer.to_account_id = createTransferDto.to_account_id;
-      transfer.amount = createTransferDto.amount;
-
-      await queryRunner.manager.save(fromEntry)
-      await queryRunner.manager.save(toEntry)
-      await queryRunner.manager.save(fromAccount)
-      await queryRunner.manager.save(toAccount)
-      await queryRunner.manager.save(transfer)
-
-      await queryRunner.commitTransaction()
-
-      res = {
-        from_account: fromAccount,
-        to_account: toAccount,
-        from_entry: fromEntry,
-        to_entry: toEntry,
-        transfer: transfer
-      }
-    } catch (error) {
-      await queryRunner.rollbackTransaction()
-      throw error
-    } finally {
-      await queryRunner.release()
     }
 
     return res
